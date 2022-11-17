69818043c36fd6287bcffaba0a9805e5a9bf77c7
Desmond <desmondfon@yahoo.com>
2022-11-17 16:14:38 +0100

    implemented some functionalities

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const check = document.getElementsByClassName("check");
+const check = document.querySelectorAll(".check");
 const tick = document.getElementsByClassName("tick");
 const moon = document.getElementById("moon-icon");
 const sun = document.getElementById("sun-icon");
@@ -69,7 +69,6 @@ elem.addEventListener("keypress", (event)=> {
       event.preventDefault();
       myTodo.push(elem.value);
       console.log(myTodo);
-      // console.log(elem.value);
       let listItems = '';
       for(let i = 0; i <myTodo.length; i++) {
         
@@ -78,16 +77,11 @@ elem.addEventListener("keypress", (event)=> {
       >
         <span class="flex items-center">
           <span class="border p-1 rounded-full w-5 h-5 mr-2 check dark:border-gray-600" onclick="print()">
-            <img
-              src="images/icon-check.svg"
-              alt=""
-              class="w-3 hidden tick" id="tick"
-            />
           </span>
-          <p id="items">${myTodo[i]}</p>
+          <p class="items">${myTodo[i]}</p>
         </span>
         <span>
-          <img src="images/icon-cross.svg" alt="" class="w-3"/>
+          <img src="images/icon-cross.svg" alt="" class="w-3 cross"/>
         </span>
       </li>
       <div class="w-full h-[1px] bg-gray-200 dark:bg-gray-600 "></div>`
@@ -95,65 +89,61 @@ elem.addEventListener("keypress", (event)=> {
       list.innerHTML = listItems;
       elem.value = '';
       itemsLeft.innerHTML = `${myTodo.length} items left`
-      console.log(document.getElementById('items').textContent);
+      // console.log(document.getElementById('items').textContent);
     }
   });
 
-// if (document.getElementById("tick").classList.contains("hidden")){
-//   console.log('hidden');
-// } else {
-//   completed.push(elem.value)
-//   console.log(completed);
-// }
-
-
-function print() {
-  Array.from(document.getElementsByClassName("check")).forEach((copyy) => {
-    if (document.getElementById("tick").classList.contains("hidden")) {
-      document.getElementById("tick").classList.remove("hidden");
-      copyy.setAttribute(
-        "style",
-        "background: linear-gradient(to bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%));"
-      );
-      document.getElementById('items').setAttribute('style', "text-decoration: line-through;")
-      completedArray.push(document.getElementById('items').textContent)
-      console.log(completedArray);
-    } else {
-      document.getElementById("tick").classList.add("hidden");
-      copyy.setAttribute("style", "background: none;");
-      document.getElementById('items').setAttribute('style', "text-decoration: none;")
-      activeArray.push(document.getElementById('items').textContent)
-      console.log(activeArray);
-      // copyy.classList.remove('hidden')
+  function print() {
+Array.from(document.getElementsByClassName("check")).forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    const targetElement = e.target;
+    if (targetElement.matches(".check")){
+      // console.log('click');
+      // console.log(btn.nextSibling.nextSibling);
+      btn.setAttribute("style","background: linear-gradient(to bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%));", "color: hsl(234 39% 85% / var(--tw-text-opacity));")
+      btn.innerHTML = `<img
+      src="images/icon-check.svg"
+      alt=""
+      class="w-3"
+    />`;
+    btn.nextSibling.nextSibling.setAttribute("style","text-decoration: line-through; color: gray");
+      completedArray.push(btn.nextSibling.nextSibling.textContent)
+      // console.log(completedArray);
+    } else{
+      btn.setAttribute("style", "background: none");
+      btn.nextSibling.nextSibling.setAttribute("style","text-decoration: none; color: hsl(235 19% 35% / var(--tw-text-opacity));");
+      btn.innerHTML = `<img
+      src="images/icon-cross.svg" class="hidden">`;
+      activeArray.push(btn.nextSibling.nextSibling.textContent);
+      // console.log(activeArray);
     }
-  });
-}
-
-all.addEventListener('click', ()=> {
-  let listItems = '';
-      for(let i = 0; i <myTodo.length; i++) {
-        
-        listItems += `<li
-        class="flex justify-between py-4 px-5 text-xs items-center text-veryDarkGrayishBlueLT dark:text-lightGrayishBlueDM"
-      >
-        <span class="flex items-center">
-          <span class="border p-1 rounded-full w-5 h-5 mr-2 check dark:border-gray-600" onclick="print()">
-            <img
-              src="images/icon-check.svg"
-              alt=""
-              class="w-3 hidden tick" id="tick"
-            />
-          </span>
-          <p id="items">${myTodo[i]}</p>
-        </span>
-        <span>
-          <img src="images/icon-cross.svg" alt="" class="w-3"/>
-        </span>
-      </li>
-      <div class="w-full h-[1px] bg-gray-200 dark:bg-gray-600 "></div>`
-      }
-      list.innerHTML = listItems;
+  })
 })
+  }
+
+
+
+all.addEventListener("click", ()=> {
+    let listItems = '';
+    for(let i = 0; i <myTodo.length; i++) {
+      
+      listItems += `<li
+      class="flex justify-between py-4 px-5 text-xs items-center text-veryDarkGrayishBlueLT dark:text-lightGrayishBlueDM todo-item" draggable="true"
+    >
+      <span class="flex items-center">
+        <span class="border p-1 rounded-full w-5 h-5 mr-2 check dark:border-gray-600" onclick="print()">
+        </span>
+        <p class="items">${myTodo[i]}</p>
+      </span>
+      <span>
+        <img src="images/icon-cross.svg" alt="" class="w-3 cross"/>
+      </span>
+    </li>
+    <div class="w-full h-[1px] bg-gray-200 dark:bg-gray-600 "></div>`
+    }
+    list.innerHTML = listItems;
+  }
+);
 
 active.addEventListener('click', ()=> {
   let listItems = '';
@@ -193,7 +183,7 @@ completed.addEventListener('click', ()=> {
           <p id="items">${completedArray[i]}</p>
         </span>
         <span>
-          <img src="images/icon-cross.svg" alt="" class="w-3"/>
+          <img src="images/icon-cross.svg" alt="" class="w-3 cross"/>
         </span>
       </li>
       <div class="w-full h-[1px] bg-gray-200 dark:bg-gray-600 "></div>`
